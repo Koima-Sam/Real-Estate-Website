26c02846dcdcf985ab0d86104d7078f67a37f927
Koima-Sam <sam17koima@gmail.com>
2023-10-17 15:46:31 +0300

    handle toggle and media query

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import './header.css'
 import logo from "../../images/logo.png";
 import { Link } from "react-router-dom";
 import { nav } from "../../data/Data";
 const Header = () => {
+  const [navlist, setNavList] = useState(false)
   return (
+    
     <>
       <header>
         <div className="container flex">
@@ -12,7 +14,7 @@ const Header = () => {
             <img src={logo} alt="logo" />
           </div>
           <div className="nav">
-            <ul className="flex">
+            <ul className={navlist? "small" :"flex"}>
               {nav.map((list, index) => (
                 <li key={index}>
                   <Link to={list.path}>{list.text}</Link>
@@ -24,12 +26,14 @@ const Header = () => {
             <h4>
               <span>2</span> My List
             </h4>
-            <button className="btn1">
+            <button className="btn1" >
               <i className="fa fa-sign-out"></i>Sign In
             </button>
           </div>
           <div className="toggle">
-            <i className="fa fa-bars"></i>
+            <button onClick={()=>setNavList(!navlist)}>
+              {navlist? <i className="fa fa-times"></i>:<i className="fa fa-bars"></i>}
+            </button>
           </div>
         </div>
       </header>
