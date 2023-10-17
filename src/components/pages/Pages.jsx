c67339982539da1a9e35cbf26aeb0c2e1c12597a
Koima-Sam <sam17koima@gmail.com>
2023-10-17 14:56:51 +0300

    handle routes

---
+++ src/components/pages/Pages.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Header from '../common/header/Header'
+
+const Pages = () => {
+  return (
+    <>
+      <Routes>
+        <Header />
+      </Routes>
+    </>
+  )
+}
+
+export default Pages
