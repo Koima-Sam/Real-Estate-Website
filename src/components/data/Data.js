2cfe86204e0b28ef79fabf9ffd967a91c13c7f3d
Koima-Sam <sam17koima@gmail.com>
2023-10-18 12:18:01 +0300

    add featured card and component

--- src/components/data/Data.js
+++ src/components/data/Data.js
@@ -26,27 +26,27 @@ export const nav = [
   ]
   export const featured = [
     {
-      cover: "../images/hero/h1.png",
+      cover: '/images/hero/h1.png',
       name: "Family House",
       total: "122 Property",
     },
     {
-      cover: "../images/hero/h2.png",
+      cover: "/images/hero/h2.png",
       name: "House & Villa",
       total: "155 Property",
     },
     {
-      cover: "../images/hero/h3.png",
+      cover: "/images/hero/h3.png",
       name: "Apartment",
       total: "300 Property",
     },
     {
-      cover: "../images/hero/h4.png",
+      cover: "../../images/hero/h4.png",
       name: "Office & Studio",
       total: "80 Property",
     },
     {
-      cover: "../images/hero/h6.png",
+      cover: "/images/hero/h6.png",
       name: "Villa & Condo",
       total: "80 Property",
     },
