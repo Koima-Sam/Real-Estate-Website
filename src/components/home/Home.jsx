6e3ba9a9a69cea0fee00be418b683ccaf2c04b9d
Koima-Sam <sam17koima@gmail.com>
2023-10-18 16:00:11 +0300

    add recent card and component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './hero/Hero'
 import Featured from './featured/Featured'
+import Recent from './recent/Recent'
 
 const Home = () => {
   return (
     <div>
     <Hero />
     <Featured />
+    <Recent />
     </div>
   )
 }
---
+++ src/components/home/recent/Recent.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Heading from '../../common/header/Heading'
+import RecentCard from './RecentCard'
+import './recent.css'
+const Recent = () => {
+  return (
+    <>
+    <section className="recent padding">
+        <div className="container">
+            <Heading  title='Recent Property Listed' subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.' />
+            <RecentCard />
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default Recent
