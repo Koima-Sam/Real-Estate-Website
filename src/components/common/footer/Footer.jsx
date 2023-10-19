8bd8271d3d2ca4ec73c823f216280a36126a3984
Koima-Sam <sam17koima@gmail.com>
2023-10-19 16:59:34 +0300

    add footer

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
-
+import logo from '../../images/logo-light.png'
+import { footer } from '../../data/Data'
+import './footer.css'
 const Footer = () => {
   return (
-    <>Footer</>
+    <>
+    <section className="footerContact">
+        <div className="container">
+            <div className="send flex">
+                <div className="text">
+                    <h1>Do You Have Questions ?</h1>
+                    <p>We'll help market your real estate ambitions</p>
+                </div>
+                <button className="btn5">Contact us Today</button>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div className="container">
+            <div className="box">
+                <div className="logo">
+                    <img src={logo} alt="" />
+                    <h2>Do You Need Help With Anything?</h2>
+              <p>Receive updates, hot deals, tutorials, discounts sent straignt in your inbox every month</p>
+              <div className="input flex">
+                <input type="text" placeholder='Email Address' name='' id='' />
+                <button>Subscribe</button>
+              </div>
+                </div>
+            </div>
+            {footer.map((item, index) =>(
+                <div className="box" key={index}>
+                    <h3>{item.title}</h3>
+                    <ul>
+                        {item.text.map((val,index) =>(
+                            <li key={index}>{val.list}</li>
+                        ))}
+                    </ul>
+                </div>
+            ))}
+        </div>
+    </footer>
+    <div className="legal">
+        <span>© {new Date().getFullYear()} Credits to KoimaSam.</span>
+    </div>
+    </>
   )
 }
 
