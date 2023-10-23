8164526ea4d237c322c2f9f6f478325f84563866
Koima-Sam <sam17koima@gmail.com>
2023-10-23 11:56:20 +0300

    route the pages

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -4,6 +4,10 @@ import Header from '../common/header/Header'
 import Home from '../home/Home'
 import Footer from '../common/footer/Footer'
 import About from '../about/About'
+import Services from '../services/Services'
+import Blog from '../blog/Blog'
+import Pricing from '../pricing/Pricing'
+import Contact from '../contact/Contact'
 
 const Pages = () => {
   return (
@@ -12,6 +16,10 @@ const Pages = () => {
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route exact path='/about' element={<About />} />
+        <Route exact path='/services' element={<Services/>} />
+        <Route exact path='/blog' element={<Blog/>} />
+        <Route exact path='/pricing' element={<Pricing/>} />
+        <Route exact path='/contact' element={<Contact/>} />
       </Routes>
       <Footer />
     </>
