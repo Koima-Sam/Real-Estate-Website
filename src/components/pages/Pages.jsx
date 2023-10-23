5f50a2bac659f6c7bdd09e18ab3ac7a916623292
Koima-Sam <sam17koima@gmail.com>
2023-10-23 10:57:19 +0300

    route about page

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -3,14 +3,15 @@ import { Route, Routes } from 'react-router-dom'
 import Header from '../common/header/Header'
 import Home from '../home/Home'
 import Footer from '../common/footer/Footer'
+import About from '../about/About'
 
 const Pages = () => {
   return (
     <>
      <Header /> 
       <Routes>
-       
         <Route exact path='/' element={<Home />} />
+        <Route exact path='/about' element={<About />} />
       </Routes>
       <Footer />
     </>
